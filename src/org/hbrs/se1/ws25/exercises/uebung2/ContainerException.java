804d4d93b9e6ff3e9d5c19fe103b7aa320f6cc50
spezi <spezius@proton.me>
2025-10-16 09:31:18 +0200

    Formatierung

--- src/org/hbrs/se1/ws25/exercises/uebung2/ContainerException.java
+++ src/org/hbrs/se1/ws25/exercises/uebung2/ContainerException.java
@@ -1,6 +1,6 @@
 package org.hbrs.se1.ws25.exercises.uebung2;
 
-public class ContainerException extends RuntimeException{
+public class ContainerException extends Exception{
 
     public ContainerException(int memberid){
         super("Das Member-Objekt mit der ID "+ memberid + " ist bereits vorhanden");
