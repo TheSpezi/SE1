104779b0b2b4786a9bca25262d2ccef8d08a55c8
spezi <spezius@proton.me>
2025-10-22 11:13:26 +0200

    Besseres Exception handling

--- src/org/hbrs/se1/ws25/exercises/uebung3/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se1/ws25/exercises/uebung3/persistence/PersistenceStrategyStream.java
@@ -26,12 +26,10 @@ public class PersistenceStrategyStream<E> implements PersistenceStrategy<E> {
             FileOutputStream fos = new FileOutputStream(location);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(member);
-            System.out.println("geschrieben!");
             oos.close();
             fos.close();
         } catch (Exception e){
-            e.printStackTrace();
-            throw new PersistenceException(PersistenceException.ExceptionType.ConnectionNotAvailable, e.getMessage());
+            throw new PersistenceException(PersistenceException.ExceptionType.WriteNotSuccessful, e.getMessage());
         }
 
 
@@ -44,32 +42,12 @@ public class PersistenceStrategyStream<E> implements PersistenceStrategy<E> {
      * Take also a look at the import statements above ;-!
      */
     public List<E> load() throws PersistenceException  {
-        // Some Coding hints ;-)
-
-        // ObjectInputStream ois = null;
-        // FileInputStream fis = null;
-        // List<...> newListe =  null;
-        //
-        // Initiating the Stream (can also be moved to method openConnection()... ;-)
-        // fis = new FileInputStream( " a location to a file" );
-
-        // Tipp: Use a directory (ends with "/") to implement a negative test case ;-)
-        // ois = new ObjectInputStream(fis);
-
-        // Reading and extracting the list (try .. catch ommitted here)
-        // Object obj = ois.readObject();
-
-        // if (obj instanceof List<?>) {
-        //       newListe = (List) obj;
-        // return newListe
-
-        // and finally close the streams
 
         try{
             FileInputStream fis = new FileInputStream(location);
             ObjectInputStream ois = new ObjectInputStream(fis);
             Object o = ois.readObject();
-            List<E> newListe = new ArrayList<>();
+            List<E> newListe;
 
             if(o instanceof  List<?>){
                 newListe = (List<E>) o;
@@ -79,8 +57,7 @@ public class PersistenceStrategyStream<E> implements PersistenceStrategy<E> {
             ois.close();
             fis.close();
         }catch(Exception e){
-            e.printStackTrace();
-            throw new PersistenceException(PersistenceException.ExceptionType.ConnectionNotAvailable, e.getMessage());
+            throw new PersistenceException(PersistenceException.ExceptionType.ReadNotSuccessful, e.getMessage());
 
         }
 
